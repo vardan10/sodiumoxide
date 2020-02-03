5a1d365344e68eb1f8662c5c83ef5c8a0a74a15d
Vardan Nadkarni <vardan.nadkarni@gmail.com>
2020-02-03 12:28:24 +0530

    Update lib.rs

--- libsodium-sys/src/lib.rs
+++ libsodium-sys/src/lib.rs
@@ -4,7 +4,20 @@
 #![allow(non_snake_case)]
 #![allow(clippy::all)] // we can't control bindgen output to make clippy happy
 
+#[cfg(not(all(target_arch = "wasm32", not(target_os = "emscripten"))))]
 extern crate libc;
+
+#[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
+mod libc {
+    pub type c_void = u8;
+    pub type c_int = i32;
+    pub type c_ulonglong = u64;
+    pub type c_char = i8;
+    pub type size_t = usize;
+    pub type uint64_t = u64;
+}
+
+use libc::{c_void, c_int, c_ulonglong, c_char, size_t, uint64_t};
 
 mod sodium_bindings;
 pub use sodium_bindings::*;
